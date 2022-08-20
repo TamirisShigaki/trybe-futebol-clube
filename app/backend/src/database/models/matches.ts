8730b818d4c23a282177c301d7ed735f8d217d39
Tamiris Shigaki <shigakiendo@gmail.com>
2022-08-20 08:01:53 -0300

    feat: :sparkles: criando models - matches

---
+++ app/backend/src/database/models/matches.ts
@@ -0,0 +1,54 @@
+import { BOOLEAN, INTEGER, Model } from 'sequelize';
+import db from '.';
+import Teams from './teams';
+
+class Matches extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: boolean;
+}
+
+Matches.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  modelName: 'matches',
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'homeTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'homeTeam' });
+
+Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'awayTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'awayTeam' });
+
+export default Matches;
