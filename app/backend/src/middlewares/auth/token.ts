af3e63f68acd1d1fcaef4ae3f98194881d296241
Tamiris Shigaki <shigakiendo@gmail.com>
2022-08-20 12:51:51 -0300

    feat: :sparkles: criando validação do token

--- app/backend/src/middlewares/auth/token.ts
+++ app/backend/src/middlewares/auth/token.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config';
+import { Request, Response, NextFunction } from 'express';
 import * as JWT from 'jsonwebtoken';
+import { StatusCodes } from 'http-status-codes';
 import IUser from '../../interfaces/user.interface';
+import CustomError from '../CustomError';
 
 const secret = process.env.JWT_SECRET || 'senhasecreta';
 
@@ -14,5 +17,21 @@ export default class Token {
     const token = JWT.sign({ data }, secret, jwtConfig);
 
     return token;
+  }
+
+  static validateToken(req: Request, res: Response, next: NextFunction) {
+    try {
+      const token = req.headers.authorization;
+
+      if (!token) {
+        throw new CustomError(StatusCodes.BAD_REQUEST, 'Token notFound');
+      }
+
+      const data = JWT.verify(token, secret);
+      req.body.user = data;
+      return next();
+    } catch (error) {
+      throw new CustomError(StatusCodes.UNAUTHORIZED, 'Token invalid');
+    }
   }
 }
