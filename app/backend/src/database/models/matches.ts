4c9133adc3b2f22a8bb9ba777500f646fbe31627
Tamiris Shigaki <shigakiendo@gmail.com>
2022-08-21 02:23:35 -0300

    refactor: :recycle: refatoração das associações de Macthes

--- app/backend/src/database/models/matches.ts
+++ app/backend/src/database/models/matches.ts
@@ -46,9 +46,9 @@ Matches.init({
 });
 
 Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'homeTeam' });
-Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'homeTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
 
 Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'awayTeam' });
-Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'awayTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
 
 export default Matches;
