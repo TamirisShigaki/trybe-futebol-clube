9455ea5d0f7be4c8ac6b1a4b631f2e8ab2c84258
Tamiris Shigaki <shigakiendo@gmail.com>
2022-08-21 20:57:48 -0300

    feat: :sparkles: criando rota /matches
    possivel salvar uma partida com o status inProgress - true

--- app/backend/src/services/matche.service.ts
+++ app/backend/src/services/matche.service.ts
@@ -1,17 +1,31 @@
 import Matches from '../database/models/matches';
 import Teams from '../database/models/teams';
+import IMatche from '../interfaces/matche.interface';
 import 'dotenv/config';
 
-export default class MactheService {
+export default class MatcheService {
   constructor(private modelM = Matches) {
     this.modelM = modelM;
   }
 
-  public async getMacthe() {
+  public async getMatche() {
     const result = await this.modelM.findAll({ include: [
       { model: Teams, as: 'teamHome', attributes: ['teamName'] },
       { model: Teams, as: 'teamAway', attributes: ['teamName'] },
     ] });
+
+    return result;
+  }
+
+  public async createMatche(data: IMatche) {
+    const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = data;
+    const result = await this.modelM.create({
+      homeTeam,
+      homeTeamGoals,
+      awayTeam,
+      awayTeamGoals,
+      inProgress: true,
+    });
 
     return result;
   }
