ccd434b1c64766176144bdd9576cabaf7ab283af
Tamiris Shigaki <shigakiendo@gmail.com>
2022-08-21 22:16:35 -0300

    feat: :sparkles: verificação pra não inserir times iguais

--- app/backend/src/services/matche.service.ts
+++ app/backend/src/services/matche.service.ts
@@ -1,6 +1,8 @@
+import { StatusCodes } from 'http-status-codes';
 import Matches from '../database/models/matches';
 import Teams from '../database/models/teams';
 import IMatche from '../interfaces/matche.interface';
+import CustomError from '../middlewares/CustomError';
 import 'dotenv/config';
 
 export default class MatcheService {
@@ -19,6 +21,14 @@ export default class MatcheService {
 
   public async createMatche(data: IMatche) {
     const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = data;
+
+    if (homeTeam === awayTeam) {
+      throw new CustomError(
+        StatusCodes.UNAUTHORIZED,
+        'It is not possible to create a match with two equal teams',
+      );
+    }
+
     const result = await this.modelM.create({
       homeTeam,
       homeTeamGoals,
