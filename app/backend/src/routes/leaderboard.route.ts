f73deaef8f31b2e87ed884147f04a2ed37c2dbe2
Tamiris Shigaki <shigakiendo@gmail.com>
2022-08-23 18:00:05 -0300

    feat: :sparkles: criando rotas GET /leaderboard/away e GET /leaderboard
    que retorna a tabela dos jogos fora de casa e de todos os jogos

--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -3,8 +3,11 @@ import LeaderBoardController from '../controllers/leaderboard.controller';
 import 'express-async-errors';
 
 const router = express.Router();
-const leaderBoardController = new LeaderBoardController();
 
-router.get('/leaderboard/home', (req, res) => leaderBoardController.table(req, res));
+const controller = new LeaderBoardController();
+
+router.get('/leaderboard/home', (req, res) => controller.getHome(req, res));
+router.get('/leaderboard/away', (req, res) => controller.getAway(req, res));
+router.get('/leaderboard', (req, res) => controller.getAll(req, res));
 
 export default router;
